ce3d4edb0d6fe71fa268462f69be68b080c3d586
Paul TCHAA <paultchaa@gmail.com>
2018-06-26 11:52:40 +0100

    Implementing the submitting event to call the makeGrid() with the appropriate grid size

--- js/designs.js
+++ js/designs.js
@@ -2,3 +2,19 @@
 var colorInput = $('#colorPicker'); 
 // Select size input
 var hauteur, largeur;
+
+// When size is submitted by the user, call makeGrid()
+
+$('#sizePicker').submit(function(e){
+				// to avoid being redirected to another page..
+		e.preventDefault();	
+			    // get the values for grid size 
+		hauteur = $('#inputHeight').val();
+		largeur = $('#inputWeight').val();
+				// then, call the makeGrid() to create and apply colors to the squares
+		makeGrid(hauteur, largeur);
+});
+
+function makeGrid(hauteur, largeur) {
+		
+}	// end makeGrid()
