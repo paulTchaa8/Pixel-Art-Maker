71a2b625e68c3a7555a284128818c01d350c97f4
Paul TCHAA <paultchaa@gmail.com>
2018-06-26 12:10:20 +0100

    Creating a grid of squares

--- js/designs.js
+++ js/designs.js
@@ -8,7 +8,7 @@ var hauteur, largeur;
 $('#sizePicker').submit(function(e){
 				// to avoid being redirected to another page..
 		e.preventDefault();	
-			    // get the values for grid size 
+			    // Here we get the grid size 
 		hauteur = $('#inputHeight').val();
 		largeur = $('#inputWeight').val();
 				// then, call the makeGrid() to create and apply colors to the squares
@@ -16,5 +16,23 @@ $('#sizePicker').submit(function(e){
 });
 
 function makeGrid(hauteur, largeur) {
+	
+// Let's remove the previous grid lines and its descendants to show the new grid..
+		$('tr').remove(); 
 		
+		var	table = $('#pixelCanvas');
+// Now we display the new grid, with a for loop.. 		
+		for(var h=0; h<hauteur; h++)
+		{
+				// we insert a new line - a 'tr' element here..
+			table.append('<tr></tr>');
+				// a nested for loop for the cells 
+			for(var w=0; w<largeur; w++)
+			{
+				// same, new cells are added in the LAST 'tr' element - the 'td' elements..
+			$('tr:last').append('<td></td>');
+			}
+		}
+
+
 }	// end makeGrid()
