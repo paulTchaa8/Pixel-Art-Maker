61eb0c17ff35a6c6ade4f886dbb9854ef51b0d06
Paul TCHAA <paultchaa@gmail.com>
2018-06-26 12:36:49 +0100

    Define the click Event upon a square to Apply or Remove a color on it.

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,7 @@
 // Select color input
 var colorInput = $('#colorPicker'); 
 // Select size input
-var hauteur, largeur;
+var hauteur = $('#inputHeight'), largeur = $('#inputWeight');
 
 // When size is submitted by the user, call makeGrid()
 
@@ -9,30 +9,45 @@ $('#sizePicker').submit(function(e){
 				// to avoid being redirected to another page..
 		e.preventDefault();	
 			    // Here we get the grid size 
-		hauteur = $('#inputHeight').val();
-		largeur = $('#inputWeight').val();
+		var Hauteur = hauteur.val();
+		var Largeur = largeur.val();
 				// then, call the makeGrid() to create and apply colors to the squares
-		makeGrid(hauteur, largeur);
+		makeGrid(Hauteur, Largeur);
 });
 
-function makeGrid(hauteur, largeur) {
+function makeGrid(Hauteur, Largeur) {
 	
 // Let's remove the previous grid lines and its descendants to show the new grid..
 		$('tr').remove(); 
 		
 		var	table = $('#pixelCanvas');
 // Now we display the new grid, with a for loop.. 		
-		for(var h=0; h<hauteur; h++)
+		for(var h=0; h<Hauteur; h++)
 		{
 				// we insert a new line - a 'tr' element here..
 			table.append('<tr></tr>');
 				// a nested for loop for the cells 
-			for(var w=0; w<largeur; w++)
+			for(var w=0; w<Largeur; w++)
 			{
 				// same, new cells are added in the LAST 'tr' element - the 'td' elements..
 			$('tr:last').append('<td></td>');
 			}
 		}
 
+	table.on('click', 'td', function(){
+// We use the << event Delegation >>, to apply the click event to each cell 'td' on the 'table'
+// Apply or Remove the color to the chosen cell - the <td> element..
+		var couleur = colorInput.val();
+		
+		if($(this).attr('style'))
+		{	
+				// the style for the color already existing, remove this atrib to get out the color..
+			$(this).removeAttr('style');
+			
+		}else{
+				// the square has no style attrib yet, let it get the style attrib with the color 
+			$(this).attr('style', 'background-color:'+couleur);
+		}
+	});
 
 }	// end makeGrid()
